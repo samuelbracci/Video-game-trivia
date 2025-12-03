9cc38b0c736bdba20b230558b4fb977451b6ed56
Sam <samuel.bracci58@ed.cna.nl.ca>
2025-12-02 20:37:33 -0330

    As of now, the final version of the game! -Lets the user start a new game when they finish -Cleaned up lots of the code, put the main 15 questions into another file to create less bloat in trivia.js, also got rid of old code I was not using and moved the Event listeners around
    Overall im really happy how this all turned out :)

--- GameTrivia/trivia.js
+++ GameTrivia/trivia.js
@@ -1,3 +1,4 @@
+import questions from "./trivialib.js"
 "use strict";
 
 const getElement = selector => document.querySelector(selector);
@@ -10,7 +11,26 @@ const answerLabels = document.querySelectorAll(".answer .label")
 const answerContainers = document.querySelectorAll(".answer")
 const formArea = document.querySelector("#question-form")
 const submitQuestion = document.querySelector("#submit-question")
+const playAgainButton = document.querySelector("#play-again-btn")
 
+
+//Game stats
+let gameQuestions = [];
+let currentIndex = 0;
+let score = 0;
+
+//Big questions variable 
+
+function getQuestions(questions, num = 10) {
+    const shuffle = [...questions]
+    for (let i = shuffle.length - 1; i > 0; i--) {
+        const position = Math.floor(Math.random() * (i + 1));
+        [shuffle[i], shuffle[position]] = [shuffle[position], shuffle[i]]
+    }
+    return shuffle.slice(0, num)
+};
+
+//Event listeners
 addButton.addEventListener("click", () => {
     startButton.style.display = "none"
     addButton.style.display = "none"
@@ -45,108 +65,18 @@ submitQuestion.addEventListener("click", () => {
     formArea.style.display = "none";
     startButton.style.display = "inline-block";
     addButton.style.display = "inline-block";
-    answerContainers.forEach(btn => btn.style.display = "inline-block");
     questionHeader.textContent = "Welcome to the Video Game Trivia website!";
 
 })
 
-//Game stats
-let gameQuestions = [];
-let currentIndex = 0;
-let score = 0;
-let toggleButtons = true;
-
-//Big questions variable 
-let questions = [
-    {
-        question: "What color is Pikachu from Pokemon?",
-        options: ["Blue", "Orange", "Yellow", "Black"],
-        answer: "Yellow"
-    },
-    {
-        question: "What is the most sold video game of all time?",
-        options: ["Minecraft", "Persona 5", "GTA 5", "Skyrim"],
-        answer: "Minecraft"
-    },
-    {
-        question: "What console does Sony make?",
-        options: ["Switch", "Steam Deck", "Xbox", "Playstation"],
-        answer: "Playstation"
-    },
-    {
-        question: "What was the first video game ever created?",
-        options: ["Tennis for Two", "Donkey Kong", "Super Mario Bros", "Pong"],
-        answer: "Tennis for Two"
-    },
-    {
-        question: "Which was the first ever video game console ever made and sold commercially?",
-        options: ["NES (Nintendo Entertainment System)", "Magnavox Odyssey", "Atari", "Gameboy"],
-        answer: "Magnavox Odyssey"
-    },
-    {
-        question: "What is Sonics signature ability?",
-        options: ["To cast magic powers", "To jump high and stomp on enemies", "To run really fast", "To play poker really well"],
-        answer: "To run really fast"
-    },
-    {
-        question: "What is Marios profession?",
-        options: ["Chef", "Accountant", "Plumber", "Prince"],
-        answer: "Plumber"
-    },
-    {
-        question: "Which of these video game series is known to be very hard?",
-        options: ["Dark Souls", "Kirby", "Minecraft", "Mario"],
-        answer: "Dark Souls"
-    },
-    {
-        question: "Which game first introduced the character Waluigi?",
-        options: ["Mario Party 3", "Mario Tennis", "Mario Kart: Double Dash", "Super Smash Bros Melee"],
-        answer: "Mario Tennis"
-    },
-    {
-        question: "What color is most associated with Nintendos brand?",
-        options: ["Dark Blue", "Green", "Yellow", "Red"],
-        answer: "Red"
-    },
-    {
-        question: "Which of the following is NOT a Nintendo console?",
-        options: ["Switch", "Gamecube", "Xbox", "Wii"],
-        answer: "Xbox"
-    },
-    {
-        question: "What is the name of the princess that Mario rescues time and time again?",
-        options: ["Peach", "Zelda", "Samus", "Daisy"],
-        answer: "Peach"
-    },
-    {
-        question: "Which is the most popular puzzle game of all time?",
-        options: ["Warios woods", "Picross", "Dr.Mario", "Tetris"],
-        answer: "Tetris"
-    },
-    {
-        question: "What item does Mario use to power up?",
-        options: ["Chaos Emeralds", "Mushroom", "Diamond sword", "Soul"],
-        answer: "Mushroom"
-    },
-    {
-        question: "Which of the following is NOT a character from Team Fortress 2?",
-        options: ["The Scout", "The Medic", "The Knight", "The Spy"],
-        answer: "The Knight"
-    }
-]
-
-function getQuestions(questions, num = 10) {
-    const shuffle = [...questions]
-    for (let i = shuffle.length - 1; i > 0; i--) {
-        const position = Math.floor(Math.random() * (i + 1));
-        [shuffle[i], shuffle[position]] = [shuffle[position], shuffle[i]]
-    }
-    return shuffle.slice(0, num)
-};
+playAgainButton.addEventListener("click", () => {
+    startGame();
+    playAgainButton.style.display = "none";
+})
 
 //Starting the game
+answerContainers.forEach(btn => btn.style.display = "none")
 startButton.addEventListener("click", startGame);
-
 
 function startGame() {
     gameQuestions = getQuestions(questions, 10);
@@ -155,6 +85,7 @@ function startGame() {
     
     startButton.style.display = "none";
     addButton.style.display = "none"
+    answerContainers.forEach(btn => btn.style.display = "inline-block");
     showQuestion()
 }
 
@@ -187,10 +118,7 @@ function handleAnswer(selected) {
 function endGame() {
     questionHeader.textContent = `Game Over! You got ${score}/${gameQuestions.length} questions right!`;
     answerLabels.forEach(btn => (btn.textContent = "-----"));
-    answerContainers.forEach(btn => (btn.onclick = null));
-    // if (toggleButtons) {
-    //     addButton.addEventListener("click", startGame);
-    // } else {
-    //     startButton.addEventListener("click", startGame)
-    // }
+    answerContainers.forEach(btn => btn.style.display = "none")
+    
+    playAgainButton.style.display = "inline-block";
 }
---
+++ GameTrivia/trivialib.js
@@ -0,0 +1,80 @@
+let questions = [
+    {
+        question: "What color is Pikachu from Pokemon?",
+        options: ["Blue", "Orange", "Yellow", "Black"],
+        answer: "Yellow"
+    },
+    {
+        question: "What is the most sold video game of all time?",
+        options: ["Minecraft", "Persona 5", "GTA 5", "Skyrim"],
+        answer: "Minecraft"
+    },
+    {
+        question: "What console does Sony make?",
+        options: ["Switch", "Steam Deck", "Xbox", "Playstation"],
+        answer: "Playstation"
+    },
+    {
+        question: "What was the first video game ever created?",
+        options: ["Tennis for Two", "Donkey Kong", "Super Mario Bros", "Pong"],
+        answer: "Tennis for Two"
+    },
+    {
+        question: "Which was the first ever video game console ever made and sold commercially?",
+        options: ["NES (Nintendo Entertainment System)", "Magnavox Odyssey", "Atari", "Gameboy"],
+        answer: "Magnavox Odyssey"
+    },
+    {
+        question: "What is Sonics signature ability?",
+        options: ["To cast magic powers", "To jump high and stomp on enemies", "To run really fast", "To play poker really well"],
+        answer: "To run really fast"
+    },
+    {
+        question: "What is Marios profession?",
+        options: ["Chef", "Accountant", "Plumber", "Prince"],
+        answer: "Plumber"
+    },
+    {
+        question: "Which of these video game series is known to be very hard?",
+        options: ["Dark Souls", "Kirby", "Minecraft", "Mario"],
+        answer: "Dark Souls"
+    },
+    {
+        question: "Which game first introduced the character Waluigi?",
+        options: ["Mario Party 3", "Mario Tennis", "Mario Kart: Double Dash", "Super Smash Bros Melee"],
+        answer: "Mario Tennis"
+    },
+    {
+        question: "What color is most associated with Nintendos brand?",
+        options: ["Dark Blue", "Green", "Yellow", "Red"],
+        answer: "Red"
+    },
+    {
+        question: "Which of the following is NOT a Nintendo console?",
+        options: ["Switch", "Gamecube", "Xbox", "Wii"],
+        answer: "Xbox"
+    },
+    {
+        question: "What is the name of the princess that Mario rescues time and time again?",
+        options: ["Peach", "Zelda", "Samus", "Daisy"],
+        answer: "Peach"
+    },
+    {
+        question: "Which is the most popular puzzle game of all time?",
+        options: ["Warios woods", "Picross", "Dr.Mario", "Tetris"],
+        answer: "Tetris"
+    },
+    {
+        question: "What item does Mario use to power up?",
+        options: ["Chaos Emeralds", "Mushroom", "Diamond sword", "Soul"],
+        answer: "Mushroom"
+    },
+    {
+        question: "Which of the following is NOT a character from Team Fortress 2?",
+        options: ["The Scout", "The Medic", "The Knight", "The Spy"],
+        answer: "The Knight"
+    }
+]
+
+//Default exporting
+export default questions;
